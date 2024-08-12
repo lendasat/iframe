0632e5d669fad066e2029f855a5455a838b6f043
Lord Lendalot <lord_lendalot@lendasat.com>
2024-08-12 18:33:45 +1000

    wip: compiling error, we need the correct the correct input

--- the-boss/src/main.rs
+++ the-boss/src/main.rs
@@ -4,23 +4,29 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-
-    use bdk::bitcoin::absolute;
+    use anyhow::Result;
+    use bdk::bitcoin::bip32::ExtendedPrivKey;
     use bdk::bitcoin::key::KeyPair;
     use bdk::bitcoin::key::Secp256k1;
+    use bdk::bitcoin::psbt::Psbt;
     use bdk::bitcoin::secp256k1::rand::thread_rng;
-    use bdk::bitcoin::Amount;
-    use bdk::bitcoin::PublicKey;
-    use bdk::bitcoin::Script;
     use bdk::bitcoin::ScriptBuf;
     use bdk::bitcoin::Transaction;
     use bdk::bitcoin::TxIn;
     use bdk::bitcoin::TxOut;
+    use bdk::bitcoin::{absolute, OutPoint, Sequence, Witness};
+    use bdk::bitcoin::{ecdsa, Amount};
+    use bdk::bitcoin::{Network, PublicKey};
+    use bdk::database::MemoryDatabase;
     use bdk::miniscript::Descriptor;
+    use bdk::template::Bip84;
+    use bdk::{KeychainKind, SignOptions, Wallet};
     use bitcoin::consensus::verify_script;
+    use std::collections::HashMap;
 
     #[test]
     fn two_of_three_multisig() {
+        let network = Network::Testnet;
         let secp = Secp256k1::new();
         let mut rng = thread_rng();
 
@@ -37,9 +43,9 @@ mod tests {
         );
         let collateral_descriptor: Descriptor<PublicKey> = descriptor.parse().unwrap();
 
-        let collateral_amount = Amount::ONE_BTC.to_sat();
+        let collateral_amount_sat = Amount::ONE_BTC.to_sat();
         let collateral_output = TxOut {
-            value: collateral_amount,
+            value: collateral_amount_sat,
             script_pubkey: collateral_descriptor.script_pubkey(),
         };
 
@@ -50,13 +56,15 @@ mod tests {
             output: vec![collateral_output],
         };
 
-        // TODO: Build collateral input for `reclaim_collateral_tx`.
         let script = collateral_descriptor.explicit_script().unwrap();
         let collateral_input = TxIn {
-            previous_output: todo!(),
-            script_sig: todo!(),
-            sequence: todo!(),
-            witness: todo!(),
+            previous_output: OutPoint {
+                txid: fund_tx.txid(),
+                vout: 0,
+            },
+            script_sig: ScriptBuf::new(),
+            sequence: Sequence::ENABLE_RBF_NO_LOCKTIME,
+            witness: Witness::new(),
         };
 
         let descriptor = format!("pk({borrower})", borrower = borrower_kp.public_key(),);
@@ -73,7 +81,42 @@ mod tests {
             output: vec![reclaim_collateral_output],
         };
 
+        // TODO: sign collateral input
+        let mut sigs = HashMap::<bitcoin::PublicKey, ecdsa::Signature>::new();
+
+        let borrower_wallet = create_wallet(network, borrower_kp.secret_key().as_ref()).unwrap();
+        let the_boss_wallet = create_wallet(network, the_boss_kp.secret_key().as_ref()).unwrap();
+
+        let mut reclaim_collateral_tx_psbt = Psbt::from_unsigned_tx(reclaim_collateral_tx).unwrap();
+
+        let _ = borrower_wallet
+            .sign(&mut reclaim_collateral_tx_psbt, SignOptions::default())
+            .unwrap();
+        let finalized = the_boss_wallet
+            .sign(&mut reclaim_collateral_tx_psbt, SignOptions::default())
+            .unwrap();
+        assert!(finalized);
+
         // TODO: Check that we can spend the multisig output with 2 keys.
-        verify_script(todo!(), todo!(), todo!(), todo!());
+        let script_buf = collateral_descriptor.script_pubkey();
+        let script1 = script_buf.as_script();
+        verify_script(
+            bitcoin_units::,
+            0,
+            bitcoin_units::Amount::from_sat(collateral_amount_sat),
+            reclaim_collateral_tx_psbt.serialize().as_slice(),
+        )
+        .unwrap();
+    }
+
+    fn create_wallet(network: Network, priv_key: &[u8]) -> Result<Wallet<MemoryDatabase>> {
+        let xprv = ExtendedPrivKey::new_master(network, priv_key)?;
+        let wallet = Wallet::new(
+            Bip84(xprv, KeychainKind::External),
+            Some(Bip84(xprv, KeychainKind::Internal)),
+            network,
+            MemoryDatabase::default(),
+        )?;
+        Ok(wallet)
     }
 }
