00f2ce5eb675bea749562e74cf6de080dc2da700
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2024-08-16 21:48:07 +1000

    feat: Let borrower create and load wallet

--- borrower-wallet/src/lib.rs
+++ borrower-wallet/src/lib.rs
@@ -1,11 +1,31 @@
-use wasm_bindgen::prelude::*;
+use anyhow::Result;
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
+
+mod storage;
+mod wallet;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub fn new_wallet(passphrase: String) -> Result<(), JsValue> {
+    map_err_to_js!(wallet::new_wallet(&passphrase))
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+pub fn load_wallet(passphrase: String) -> Result<(), JsValue> {
+    map_err_to_js!(wallet::load_wallet(&passphrase))
+}
+
+#[wasm_bindgen]
+pub fn get_mnemonic() -> Result<String, JsValue> {
+    map_err_to_js!(wallet::get_mnemonic())
+}
+
+#[macro_export]
+macro_rules! map_err_to_js {
+    ($e:expr) => {
+        match $e {
+            Ok(i) => Ok(i),
+            Err(e) => Err(JsValue::from_str(&format!("{:#}", e))),
+        }
+    };
 }
