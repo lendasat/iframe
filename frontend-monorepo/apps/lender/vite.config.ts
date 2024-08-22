e6141a0fbb77aa8342282877bfa933c440622c96
Lord Lendalot <lord_lendalot@lendasat.com>
2024-08-22 16:35:26 +1000

    fix: lender frontend was missing plugin

--- frontend-monorepo/apps/lender/vite.config.ts
+++ frontend-monorepo/apps/lender/vite.config.ts
@@ -1,35 +1,44 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import {nxViteTsPaths} from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/lender',
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/lender',
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  build: {
-    outDir: '../../dist/apps/lender',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    server: {
+        port: 4200,
+        host: 'localhost',
     },
-  },
+
+    preview: {
+        port: 4300,
+        host: 'localhost',
+    },
+
+    plugins: [react(), nxViteTsPaths(), svgr({
+        svgrOptions: {
+            exportType: 'named',
+            ref: true,
+            svgo: false,
+            titleProp: true,
+        },
+        include: '**/*.svg',
+    })],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    build: {
+        outDir: '../../dist/apps/lender',
+        emptyOutDir: true,
+        reportCompressedSize: true,
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
 });
