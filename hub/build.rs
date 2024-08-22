9815746dc2eb9972028d148a401466beb9d2009f
Lord Lendalot <lord_lendalot@lendasat.com>
2024-08-22 16:33:37 +1000

    fix: generate the correct directories

--- hub/build.rs
+++ hub/build.rs
@@ -2,12 +2,12 @@ use std::fs;
 
 fn main() {
     // ensure that the directory exists which needs to be embedded in our binary
-    let directory_path = "../frontend-monorepo/dist/apps/borrower-frontend";
+    let directory_path = "../frontend-monorepo/dist/apps/borrower";
     if fs::create_dir_all(directory_path).is_err() {
         std::process::exit(1);
     }
     // ensure that the directory exists which needs to be embedded in our binary
-    let directory_path = "../frontend-monorepo/dist/apps/lender-frontend";
+    let directory_path = "../frontend-monorepo/dist/apps/lender";
     if fs::create_dir_all(directory_path).is_err() {
         std::process::exit(1);
     }
