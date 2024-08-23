bee077f8099df14d012141da6878b78940d8763b
Lord Lendalot <lord_lendalot@lendasat.com>
2024-08-23 16:19:12 +1000

    chore: enable cors for borrower
    this is needed so that we can call the endpoints when serving it in dev mode. Note: we might need to adapt the config in the future if more headers are needed.

--- hub/src/routes/borrower.rs
+++ hub/src/routes/borrower.rs
@@ -1,10 +1,19 @@
 use crate::config::Config;
 use crate::routes::AppState;
+use axum::http::header::ACCEPT;
+use axum::http::header::ACCESS_CONTROL_ALLOW_HEADERS;
+use axum::http::header::ACCESS_CONTROL_ALLOW_ORIGIN;
+use axum::http::header::AUTHORIZATION;
+use axum::http::header::CONTENT_TYPE;
+use axum::http::header::ORIGIN;
+use axum::http::HeaderValue;
+use axum::http::Method;
 use axum::Router;
 use sqlx::Pool;
 use sqlx::Postgres;
 use std::sync::Arc;
 use tokio::task::JoinHandle;
+use tower_http::cors::CorsLayer;
 
 pub(crate) mod auth;
 pub(crate) mod contracts;
@@ -23,6 +32,22 @@ pub async fn spawn_borrower_server(config: Config, db: Pool<Postgres>) -> JoinHa
         .merge(loan_offers::router(app_state.clone()))
         .merge(contracts::router(app_state.clone()))
         .merge(frontend::router());
+
+    // todo: make this a dev-only setting
+    let cors = CorsLayer::new()
+        .allow_credentials(true)
+        .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers(vec![
+            ORIGIN,
+            AUTHORIZATION,
+            ACCEPT,
+            ACCESS_CONTROL_ALLOW_HEADERS,
+            ACCESS_CONTROL_ALLOW_ORIGIN,
+            CONTENT_TYPE,
+        ])
+        .allow_origin(["http://localhost:4200".parse::<HeaderValue>().unwrap()]);
+
+    let app = app.layer(cors);
 
     let listener = tokio::net::TcpListener::bind(&config.borrower_listen_address)
         .await
