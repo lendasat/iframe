94197d0d597f5f4bcc74b87b0939af506055d5bb
Lord Lendalot <lord_lendalot@lendasat.com>
2024-08-23 13:29:37 +1000

    chore: move layout one layer down

--- frontend-monorepo/apps/borrower/src/main.tsx
+++ frontend-monorepo/apps/borrower/src/main.tsx
@@ -9,21 +9,11 @@ import App from "./app/app";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
-const navItems = [
-  { href: "/request-loan", label: "Request a Loan" },
-  { href: "/my-loans", label: "My Loans" },
-  { href: "/my-account", label: "My Account" },
-  { href: "/wallet", label: "Wallet" },
-  { href: "/help", label: "Help" },
-  { href: "/logout", label: "Logout" },
-];
 
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <Layout navItems={navItems} description={"Sell at the price you deserve"} title={"Welcome Borrower"}>
-        <App />
-      </Layout>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
