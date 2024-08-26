567613992ccb53b8cb8bce606d47704fe51e74e7
Lord Lendalot <lord_lendalot@lendasat.com>
2024-08-26 15:51:58 +1000

    feat: simple component fetching bitcoin price

--- frontend-monorepo/apps/borrower/src/app/dash-board.tsx
+++ frontend-monorepo/apps/borrower/src/app/dash-board.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PriceTicker from "./btc-usd-price-ticker";
+
+interface Price {
+  bitcoin: number;
+}
 
 function DashBoard() {
-  return <>Here comes everythings</>;
+  return <>
+    Here comes everythings
+    <PriceTicker/>
+  </>;
 }
 
 export default DashBoard;
