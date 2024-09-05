6fc8d93550c840f00fc8a94005a37a8002824df3
Lord Lendalot <lord_lendalot@lendasat.com>
2024-09-05 15:40:21 +1000

    chore: fix formatting

--- frontend-monorepo/apps/borrower/src/app/profile.tsx
+++ frontend-monorepo/apps/borrower/src/app/profile.tsx
@@ -2,7 +2,6 @@ import { faTools } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function Profile() {
-
   return (
     <div>
       <div className="d-flex flex-column justify-content-start align-items-center py-4">
