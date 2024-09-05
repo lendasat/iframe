64e8a979dd451ef52dc6b695b0f111528c5dd456
Lord Lendalot <lord_lendalot@lendasat.com>
2024-09-05 15:35:39 +1000

    chore: show wip on lender profile
    We don't have an endpoint yet

--- frontend-monorepo/apps/borrower/src/app/profile.tsx
+++ frontend-monorepo/apps/borrower/src/app/profile.tsx
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom";
+import { faTools } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function Profile() {
-  const { id } = useParams();
 
   return (
     <div>
-      <h3>User profile: {id}</h3>
+      <div className="d-flex flex-column justify-content-start align-items-center py-4">
+        <FontAwesomeIcon icon={faTools} size="4x" className="text-warning mb-3" />
+        <h3 className="text-muted">This is work in progress</h3>
+      </div>
     </div>
   );
 }
