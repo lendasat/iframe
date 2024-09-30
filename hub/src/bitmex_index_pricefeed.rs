2632c10a3096b0372dfb5cb4c343b4a682392ee8
Lord Lendalot <lord_lendalot@lendasat.com>
2024-09-30 14:03:00 +1000

    fix: ensure bitmex will reconnect after connection loss

--- hub/src/bitmex_index_pricefeed.rs
+++ hub/src/bitmex_index_pricefeed.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 use futures::TryStreamExt;
 use rust_decimal::Decimal;
 use serde::Serialize;
+use std::time::Duration;
 use time::OffsetDateTime;
 use tokio::sync::mpsc;
 
@@ -15,36 +16,44 @@ pub struct BitmexIndexPrice {
     pub timestamp: OffsetDateTime,
 }
 
+const RECONNECTION_TIMEOUT_SECONDS: u64 = 30;
+
 pub async fn subscribe_index_price(txs: [mpsc::Sender<BitmexIndexPrice>; 2]) -> Result<()> {
     tokio::spawn(async move {
-        let mut stream = bitmex_ws_client::stream(bitmex_stream::Network::Mainnet).await;
         loop {
-            match stream.try_next().await {
-                Ok(Some(Event::Instrument { data, .. })) => {
-                    for instrument in data {
-                        for tx in &txs {
-                            if let Err(err) = tx
-                                .send(BitmexIndexPrice {
-                                    market_price: instrument.market_price,
-                                    timestamp: instrument.timestamp,
-                                })
-                                .await
-                            {
-                                tracing::error!("Failed to notify channel about update {err:#}");
-                                continue;
+            let mut stream = bitmex_ws_client::stream(bitmex_stream::Network::Mainnet).await;
+            loop {
+                match stream.try_next().await {
+                    Ok(Some(Event::Instrument { data, .. })) => {
+                        for instrument in data {
+                            for tx in &txs {
+                                if let Err(err) = tx
+                                    .send(BitmexIndexPrice {
+                                        market_price: instrument.market_price,
+                                        timestamp: instrument.timestamp,
+                                    })
+                                    .await
+                                {
+                                    tracing::error!(
+                                        "Failed to notify channel about update {err:#}"
+                                    );
+                                    continue;
+                                }
                             }
                         }
                     }
-                }
-                Err(e) => {
-                    tracing::error!("Closing BitMEX WS after encountering error: {e:#}");
-                    break;
-                }
-                Ok(None) => {
-                    tracing::error!("BitMEX WS closed");
-                    break;
+                    Err(e) => {
+                        tracing::error!("Closing BitMEX WS after encountering error: {e:#}");
+                        break;
+                    }
+                    Ok(None) => {
+                        tracing::error!("BitMEX WS closed");
+                        break;
+                    }
                 }
             }
+            tracing::error!("BitMEX WS connection lost, reconnecting in {RECONNECTION_TIMEOUT_SECONDS} seconds...");
+            tokio::time::sleep(Duration::from_secs(RECONNECTION_TIMEOUT_SECONDS)).await;
         }
     });
     Ok(())
