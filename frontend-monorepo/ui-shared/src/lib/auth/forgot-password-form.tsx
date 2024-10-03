feea2c5c18f21b41854375937cd81db480814df1
dezignsensei1 <dezignsensei1@gmail.com>
2024-10-03 21:33:41 +0100

    Redesigned Forget password screen

--- frontend-monorepo/ui-shared/src/lib/auth/forgot-password-form.tsx
+++ frontend-monorepo/ui-shared/src/lib/auth/forgot-password-form.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import { ReactComponent as Logo } from "../lendasat_white_bg.svg";
+import { Form } from "react-bootstrap";
+import { ReactComponent as Logo } from "./../assets/lendasat_svg_logo.svg";
+import { Box, Button, Callout, Grid, Heading, Spinner, Text } from "@radix-ui/themes";
+import Background from "./../assets/forget-password-img.png";
+import TypeField from "../components/TypeField";
+import { IoArrowBackOutline, IoInformationCircleOutline } from "react-icons/io5";
+import { TbFingerprint } from "react-icons/tb";
+import { Link } from "react-router-dom";
+
 
 interface ForgotPasswordProps {
   handleSubmit: (email: string) => Promise<string>;
@@ -26,39 +33,111 @@ export function ForgotPasswordForm({ handleSubmit }: ForgotPasswordProps) {
   };
 
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <Row className="w-100">
-        <Col className="d-flex justify-content-center">
-          <div className="p-4 rounded border border-primary" style={{ backgroundColor: "#f8f9fa" }}>
-            <Logo height={80} width={400} className="mb-4 d-block mx-auto" />
+    <Grid className="min-h-screen lg:grid-cols-5 items-center">
+      {/* <img
+        className="absolute z-0 top-0 left-0 h-full w-full mix-blend-overlay opacity-5"
+        src={Background}
+        alt="Background"
+      /> */}
+      <Box minHeight={'100vh'} className="flex flex-col justify-between py-10 items-center px-6 md:px-10 lg:col-span-2">
 
-            {(!error && !success)
-              ? <div className="alert alert-info">Please enter your email to reset your password.</div>
-              : ""}
+        <Box className="flex items-center justify-between w-full">
+          {/* Logo */}
+          <Logo height={20} width={"auto"} className="w-fit invert " />
 
-            {error && <div className="alert alert-danger">{error}</div>}
-            {success && <div className="alert alert-success">{success}</div>}
+          <Link to={'/registration'} className="text-sm font-medium text-font-dark hover:text-purple-900 underline underline-offset-1">
+            Create an account
+          </Link>
+        </Box>
 
-            <Form onSubmit={onSubmit}>
-              <Form.Group controlId="formBasicEmail" className="mb-3">
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  className="p-3"
-                  style={{ width: "100%" }}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Form.Group>
+        <Box className="max-w-xs w-full mx-auto text-center">
+          <Box className="mx-auto h-16 w-16 mb-2 rounded-full shadow-black/10 bg-gradient-to-b from-black/5 to-6% to-white/0 border border-font/5 flex items-center justify-center">
+            <Box className="h-10 w-10 bg-white rounded-full border border-font/5 flex items-center justify-center">
+              <TbFingerprint size={20} className="text-font/50" />
+            </Box>
+          </Box>
 
-              <Button variant="primary" type="submit" className="w-100 p-2" disabled={isLoading}>
-                {isLoading ? "Loading…" : "Submit"}
+          <Heading size={'7'} mb={'1'} className="text-font-dark font-semibold">
+            Forgot password?
+          </Heading>
+
+          <Text size={'2'} weight={'medium'} className="text-font/60 text-center">
+            Worry not, we'll send you a reset instruction.
+          </Text>
+
+          <Form onSubmit={onSubmit} className="w-full mt-7 space-y-2.5">
+            {/* Email */}
+            <Box className="text-left">
+              <Text as="label" size={"1"} weight={"medium"} className="text-font/70 mb-2">Email</Text>
+              <TypeField
+                placeholder="example@domain.com"
+                value={email}
+                autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Box>
+
+            {/* Error message */}
+            {error && (
+              <Callout.Root color="tomato">
+                <Callout.Icon>
+                  <IoInformationCircleOutline />
+                </Callout.Icon>
+                <Callout.Text>
+                  {error}
+                </Callout.Text>
+              </Callout.Root>
+            )}
+
+            {/* Error message */}
+            {success && (
+              <Callout.Root color="green">
+                <Callout.Icon>
+                  <IoInformationCircleOutline />
+                </Callout.Icon>
+                <Callout.Text>
+                  {success}
+                </Callout.Text>
+              </Callout.Root>
+            )}
+
+            {/* Submit Button */}
+            <Box className="pt-4">
+              <Button
+                color="purple"
+                type="submit"
+                size={"3"}
+                variant="solid"
+                radius="large"
+                disabled={email && !isLoading ? false : true}
+                className="w-full h-12"
+              >
+                {isLoading ? <Spinner size={"3"} /> : "Register"}
               </Button>
-            </Form>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+            </Box>
+          </Form>
+
+          {/* Sign Up Routing */}
+
+          <Link to={'/login'} className="text-sm font-medium text-font-dark hover:text-purple-800">
+            <Box className="flex items-center gap-1 justify-center mt-6">
+              <IoArrowBackOutline />
+              <Text size={'2'}>Back to Sign in</Text>
+            </Box>
+          </Link>
+
+        </Box>
+
+
+        <Box className="h-10" />
+      </Box>
+      <Box className="lg:col-span-3 bg-purple-50 h-full flex items-center pl-20 rounded-l-[60px]">
+        <img
+          src={Background}
+          alt="Dashboard Screen"
+        />
+      </Box>
+    </Grid>
   );
 }
 
