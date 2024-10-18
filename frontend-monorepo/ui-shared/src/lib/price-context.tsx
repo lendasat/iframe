4f30fde9402ce56921e575d2de3e6a90513c23eb
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2024-10-18 18:23:11 +1100

    fix(frontend): Do not connect to price feed WS more than once

--- frontend-monorepo/ui-shared/src/lib/price-context.tsx
+++ frontend-monorepo/ui-shared/src/lib/price-context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import React, { createContext, ReactNode, useContext, useEffect, useRef, useState } from "react";
 
 interface PriceContextProps {
   latestPrice: number;
@@ -10,29 +10,52 @@ interface RawPriceUpdate {
 
 const PriceContext = createContext<PriceContextProps | undefined>(undefined);
 
+type WebSocketConnect = () => void;
+
 export const PriceProvider: React.FC<{ url: string; children: ReactNode }> = ({ children, url }) => {
   const [latestPrice, setLatestPrice] = useState<number | undefined>();
+  const ws = useRef<WebSocket | null>(null);
+
   useEffect(() => {
-    let websocketUrl;
-    if (url.endsWith("/")) {
-      websocketUrl = `${url}api/pricefeed`;
-    } else {
-      websocketUrl = `${url}/api/pricefeed`;
+    const connect: WebSocketConnect = () => {
+      let wsUrl;
+      if (url.endsWith("/")) {
+        wsUrl = `${url}api/pricefeed`;
+      } else {
+        wsUrl = `${url}/api/pricefeed`;
+      }
+
+      ws.current = new WebSocket(wsUrl);
+
+      ws.current.onopen = () => {
+        console.log("Connected to Lendasat price feed WS");
+      };
+
+      ws.current.onmessage = (event: MessageEvent) => {
+        const data: RawPriceUpdate = JSON.parse(event.data);
+        if (data.market_price) {
+          setLatestPrice(data.market_price);
+        }
+      };
+
+      ws.current.onerror = (e: Event) => {
+        console.log(`Got error from price feed: ${JSON.stringify(e)}`);
+      };
+
+      ws.current.onclose = () => {
+        console.log("Lendasat price feed WS closed, attempting to reconnect...");
+        setTimeout(connect, 200);
+      };
+    };
+
+    if (!ws.current) {
+      connect();
     }
 
-    const socket = new WebSocket(websocketUrl);
-
-    socket.onmessage = (event) => {
-      const data: RawPriceUpdate = JSON.parse(event.data);
-      if (data.market_price) {
-        setLatestPrice(data.market_price);
-      }
-    };
-
     return () => {
-      socket.close();
+      ws.current?.close();
     };
-  }, []);
+  }, [url]);
 
   return (
     <PriceContext.Provider value={{ latestPrice }}>
