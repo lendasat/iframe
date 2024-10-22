3914a426ab63f972c78cc657b1ce9871a4c12a70
Lord Lendalot <lord_lendalot@lendasat.com>
2024-10-22 15:23:03 +1100

    feat: introduce extra invite code for lenders
    This will allow us to have different invite codes for borrowers and lenders

--- hub/src/db/invite_code.rs
+++ hub/src/db/invite_code.rs
@@ -3,10 +3,28 @@ use anyhow::Result;
 use sqlx::Pool;
 use sqlx::Postgres;
 
-pub async fn load_invite_code(pool: &Pool<Postgres>, code: &str) -> Result<Option<InviteCode>> {
+pub async fn load_invite_code_borrower(
+    pool: &Pool<Postgres>,
+    code: &str,
+) -> Result<Option<InviteCode>> {
     let invite_code = sqlx::query_as!(
         InviteCode,
-        "SELECT id, active, code FROM INVITE_CODES WHERE code = $1",
+        "SELECT id, active, code FROM INVITE_CODES_BORROWER WHERE code = $1",
+        code
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    Ok(invite_code)
+}
+
+pub async fn load_invite_code_lender(
+    pool: &Pool<Postgres>,
+    code: &str,
+) -> Result<Option<InviteCode>> {
+    let invite_code = sqlx::query_as!(
+        InviteCode,
+        "SELECT id, active, code FROM INVITE_CODES_LENDER WHERE code = $1",
         code
     )
     .fetch_optional(pool)
