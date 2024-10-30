b9d0e2543f477059e276b11d84d5937f0c8bc658
itseghosimen <itseghosimee@gmail.com>
2024-10-30 15:28:46 +0100

    code format

--- frontend-monorepo/apps/borrower/src/app/my-loans/collateralize-contract.tsx
+++ frontend-monorepo/apps/borrower/src/app/my-loans/collateralize-contract.tsx
@@ -32,22 +32,22 @@ export function CollateralContractDetails({
 
   return (
     <Box>
-      <Heading size={'4'} weight={'medium'}>
+      <Heading size={"4"} weight={"medium"}>
         Fund Collateral Contract
       </Heading>
-      <Separator className="bg-font/10" size={'4'} my={'4'} />
+      <Separator className="bg-font/10" size={"4"} my={"4"} />
       <Box className="space-y-4">
-        <Flex align={'center'} justify={'between'}>
-          <Text size={'2'} weight={'medium'} className="text-font/70 shrink-0">
+        <Flex align={"center"} justify={"between"}>
+          <Text size={"2"} weight={"medium"} className="text-font/70 shrink-0">
             Collateral
           </Text>
 
-          <Text size={'2'} weight={'medium'} className="text-end text-font/70">
+          <Text size={"2"} weight={"medium"} className="text-end text-font/70">
             {collateral_btc.toFixed(8)} BTC
           </Text>
         </Flex>
-        <Flex align={'center'} justify={'between'}>
-          <Text size={'2'} weight={'medium'} className="text-font/70 shrink-0">
+        <Flex align={"center"} justify={"between"}>
+          <Text size={"2"} weight={"medium"} className="text-font/70 shrink-0">
             {/* TODO: here we showed the percentage as well, but we don't know the number :) */}
             Origination fee
           </Text>
@@ -57,61 +57,62 @@ export function CollateralContractDetails({
             overlay={<Tooltip>${loanOriginatorFeeUsd}</Tooltip>}
           >
             <Text
-              size={'2'}
-              weight={'medium'}
+              size={"2"}
+              weight={"medium"}
               className="text-end text-font/70"
             >
               {loanOriginatorFee.toFixed(8)} BTC
             </Text>
           </OverlayTrigger>
         </Flex>
-        <Separator className="bg-font/10" size={'4'} my={'4'} />
-        <Flex align={'center'} justify={'between'}>
-          <Text size={'2'} weight={'bold'} className="shrink-0">
+        <Separator className="bg-font/10" size={"4"} my={"4"} />
+        <Flex align={"center"} justify={"between"}>
+          <Text size={"2"} weight={"bold"} className="shrink-0">
             Total
           </Text>
 
-          <Text size={'2'} weight={'bold'} className="text-end">
+          <Text size={"2"} weight={"bold"} className="text-end">
             {totalCollateral} BTC
           </Text>
         </Flex>
-        <Separator className="bg-font/10" size={'4'} my={'4'} />
+        <Separator className="bg-font/10" size={"4"} my={"4"} />
       </Box>
-      <Box py={'4'} className="text-center">
-        <Text size={'2'} weight={'medium'} className="text-font/60">
+      <Box py={"4"} className="text-center">
+        <Text size={"2"} weight={"medium"} className="text-font/60">
           Scan QR code to make payment
         </Text>
       </Box>
-      <Flex align={'center'} justify={'center'} direction={'column'} gap={'4'}>
+      <Flex align={"center"} justify={"center"} direction={"column"} gap={"4"}>
         <Box
           onClick={() => handleCopy(collateralAddress)}
-          p={'5'}
+          p={"5"}
           className="rounded-2xl bg-white cursor-copy hover:shadow-sm"
         >
           <QRCode value={collateralAddress} size={300} />
         </Box>
         <Flex
-          align={'center'}
-          justify={'center'}
-          direction={'column'}
-          gap={'3'}
+          align={"center"}
+          justify={"center"}
+          direction={"column"}
+          gap={"3"}
         >
           <Text
-            size={'2'}
+            size={"2"}
             className="text-font/60 text-center max-w-sm font-medium"
           >
-            Please send{'  '}
+            Please send{"  "}
             <Popup
-              content={'Copy exact amount to send'}
+              content={"Copy exact amount to send"}
               className="text-font-dark font-semibold"
             >
               <span
-                onClick={()=>handleCopy(totalCollateral)}
-                className="text-font-dark font-semibold cursor-copy">
+                onClick={() => handleCopy(totalCollateral)}
+                className="text-font-dark font-semibold cursor-copy"
+              >
                 {totalCollateral} BTC
               </span>
             </Popup>
-            to{'  '}
+            to{"  "}
             <Button
               onClick={() => handleCopy(collateralAddress)}
               asChild
@@ -123,11 +124,11 @@ export function CollateralContractDetails({
               </span>
             </Button>
           </Text>
-          <Badge radius="full" color={copied ? 'green' : 'gray'}>
-            <Text size={'1'}>
+          <Badge radius="full" color={copied ? "green" : "gray"}>
+            <Text size={"1"}>
               {!copied
-                ? 'Click address/amount to copy'
-                : 'Copied to clipboard!'}
+                ? "Click address/amount to copy"
+                : "Copied to clipboard!"}
             </Text>
           </Badge>
         </Flex>
