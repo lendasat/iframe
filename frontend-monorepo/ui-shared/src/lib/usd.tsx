424c588326a1365566967e15b97a3f18d229206e
Lord Lendalot <lord_lendalot@lendasat.com>
2024-10-30 17:49:50 +1100

    chore: show interest amount in USD

--- frontend-monorepo/ui-shared/src/lib/usd.tsx
+++ frontend-monorepo/ui-shared/src/lib/usd.tsx
@@ -1,11 +1,11 @@
 import type { FC } from "react";
 
-export const formatCurrency = (value: number) => {
+export const formatCurrency = (value: number, minFraction: number = 0, maxFraction: number = 0) => {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
+    minimumFractionDigits: minFraction,
+    maximumFractionDigits: maxFraction,
   }).format(value);
 };
 
