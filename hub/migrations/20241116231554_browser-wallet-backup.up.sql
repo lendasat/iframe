737d55c44621d0e21e0917b9700d46b71c32a4b0
Lord Lendalot <lord_lendalot@lendasat.com>
2024-11-18 17:50:25 +1100

    fix: these value can be longer than 255 characters

--- hub/migrations/20241116231554_browser-wallet-backup.up.sql
+++ hub/migrations/20241116231554_browser-wallet-backup.up.sql
@@ -2,10 +2,10 @@ CREATE TABLE borrower_wallet_backups
 (
     id                  SERIAL PRIMARY KEY,
     borrower_id         CHAR(36) UNIQUE          NOT NULL,
-    passphrase_hash     VARCHAR(255)             NOT NULL,
-    mnemonic_ciphertext VARCHAR(255)             NOT NULL,
+    passphrase_hash     TEXT                     NOT NULL,
+    mnemonic_ciphertext TEXT                     NOT NULL,
     network             VARCHAR(50)              NOT NULL,
-    xpub                VARCHAR(255)             NOT NULL,
+    xpub                TEXT                     NOT NULL,
     created_at          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (borrower_id) REFERENCES borrowers (id)
 );
@@ -14,10 +14,10 @@ CREATE TABLE lender_wallet_backups
 (
     id                  SERIAL PRIMARY KEY,
     lender_id           CHAR(36) UNIQUE          NOT NULL,
-    passphrase_hash     VARCHAR(255)             NOT NULL,
-    mnemonic_ciphertext VARCHAR(255)             NOT NULL,
+    passphrase_hash     TEXT                     NOT NULL,
+    mnemonic_ciphertext TEXT                     NOT NULL,
     network             VARCHAR(50)              NOT NULL,
-    xpub                VARCHAR(255)             NOT NULL,
+    xpub                TEXT                     NOT NULL,
     created_at          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (lender_id) REFERENCES lenders (id)
 );
