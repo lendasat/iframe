ba5f8cf16f90f07d8f6a3ec1485e601a9e151072
Lord Lendalot <lord_lendalot@lendasat.com>
2024-11-26 12:22:06 +1100

    chore: add note about multisig being used

--- frontend-monorepo/apps/borrower/src/app/request-loan/steps/PayWithMoonDescriptionDialog.tsx
+++ frontend-monorepo/apps/borrower/src/app/request-loan/steps/PayWithMoonDescriptionDialog.tsx
@@ -1,6 +1,7 @@
 import type { LoanProductOption } from "@frontend-monorepo/base-http-client";
-import { AlertDialog, Box, Button, Checkbox, Flex, Separator, Text } from "@radix-ui/themes";
+import { AlertDialog, Box, Button, Callout, Checkbox, Flex, Separator, Text } from "@radix-ui/themes";
 import { useState } from "react";
+import { FaInfoCircle } from "react-icons/fa";
 import { FaTrademark } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
@@ -87,6 +88,15 @@ export const PayWithMoonDescriptionDialog = ({
                   </Text>
                 </li>
               </ul>
+              <Callout.Root color="teal" variant="soft" highContrast>
+                <Callout.Icon>
+                  <FaInfoCircle />
+                </Callout.Icon>
+                <Callout.Text>
+                  In closed beta we are using a 2-of-4 multisig contract and not DLCs. The keys are distributed among
+                  the borrower, lender, Lendasat and a trusted third party.
+                </Callout.Text>
+              </Callout.Root>
             </AlertDialog.Description>
           </Box>
           <div className="absolute bottom-0 h-7 bg-gradient-to-t from-white via-white/70 to-white/0 z-10 w-full left-0" />
