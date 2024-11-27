46a9c0c20ff0dbfeb9e5bfce5f1c54d0a44979d2
Lord Lendalot <lord_lendalot@lendasat.com>
2024-11-27 18:03:51 +1100

    fix: set default value for existing contracts

--- hub/migrations/20241127023443_change-default-value-on-contracts.up.sql
+++ hub/migrations/20241127023443_change-default-value-on-contracts.up.sql
@@ -1,2 +1,3 @@
-ALTER TABLE contracts ALTER COLUMN integration SET NOT NULL;
 ALTER TABLE contracts ALTER COLUMN integration SET DEFAULT 'StableCoin';
+UPDATE contracts set integration = 'StableCoin' where integration IS null;
+ALTER TABLE contracts ALTER COLUMN integration SET NOT NULL;
