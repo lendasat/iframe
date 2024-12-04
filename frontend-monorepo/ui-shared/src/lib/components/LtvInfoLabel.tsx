1a7297ca9d0fedbe1d5429552a83ce3116b1ca95
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2024-12-04 17:39:59 +1100

    chore: Tweak LtvInfoLabel

--- frontend-monorepo/ui-shared/src/lib/components/LtvInfoLabel.tsx
+++ frontend-monorepo/ui-shared/src/lib/components/LtvInfoLabel.tsx
@@ -10,9 +10,9 @@ export interface LtvInfoLabelProps {
 export function LtvInfoLabel({ children }: LtvInfoLabelProps) {
   return (
     <AbbreviationExplanationInfo
-      header={"Loan To Value"}
+      header={"Loan-To-Value"}
       subHeader={"LTV"}
-      description={"Loan-to-Value (LTV) ratio is used to assess the relationship between the loan amount and the value of the collateral provided. The starting LTV (for example, 50%) indicates how much Bitcoin must be placed into an escrow contract to back a certain loan amount."}
+      description={"The Loan-To-Value (LTV) ratio is the relationship between the loan amount and the value of the collateral provided. The starting LTV (for example, 50%) indicates how much Bitcoin must be placed into the collateral contract to back a certain loan amount."}
     >
       <RadixLink
         href="https://lendasat.notion.site/Frequently-Asked-Questions-100d2f24d4cf800e83bbca7cff3bb707"
