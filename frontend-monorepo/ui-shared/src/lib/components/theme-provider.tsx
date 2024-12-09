35a645701a6014613596a54c34be1c7686c1c4dc
Richard Holzeis <richard@holzeis.me>
2024-12-09 09:49:41 +0100

    refactor: Refactor theme provider
    Use only the theme provider to manage the theme state.

--- frontend-monorepo/ui-shared/src/lib/components/theme-provider.tsx
+++ frontend-monorepo/ui-shared/src/lib/components/theme-provider.tsx
@@ -1,26 +1,53 @@
 import { createContext, type ReactNode, useContext, useEffect, useState } from "react";
 
-export const getPreferredTheme = () => {
+const getPreferredSystemTheme = () => {
   return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 };
 
-const ThemeContext = createContext({
+// Order of theme preferences.
+// 1. What the user has selected in the app.
+// 2. Set in the system settings.
+// 3. Default light mode.
+export const getPreferredTheme = () => {
+  return localStorage.getItem("theme") || getPreferredSystemTheme() || "light";
+};
+
+interface Theme {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<Theme>({
   theme: "light",
-  setTheme: (theme: string) => {
-    // No-op function
+  toggleTheme: () => { // noop
   },
 });
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<string>(getPreferredTheme);
+  const [theme, setTheme] = useState<string>("light");
 
   useEffect(() => {
-    document.documentElement.classList.add(theme);
-    return () => document.documentElement.classList.remove(theme);
+    const savedTheme = getPreferredTheme();
+    applyTheme(savedTheme);
   }, [theme]);
 
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    // we only store the new theme into the local storage on toggle, as this is an in-app setting.
+    localStorage.setItem("theme", newTheme);
+    applyTheme(newTheme);
+  };
+
+  const applyTheme = (newTheme) => {
+    setTheme(newTheme);
+    // remove both light and dark classes so that only the saved theme is really used.
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add(newTheme);
+  };
+
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
