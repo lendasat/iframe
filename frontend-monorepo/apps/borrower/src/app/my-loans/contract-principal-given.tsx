4b0ee3891b8f6fb80b5169bc032720788b51773a
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2024-12-12 18:41:29 +1100

    chore(borrower): Tweak loan repayment screen
    Fixes https://github.com/lendasat/lendasat/issues/385.

--- frontend-monorepo/apps/borrower/src/app/my-loans/contract-principal-given.tsx
+++ frontend-monorepo/apps/borrower/src/app/my-loans/contract-principal-given.tsx
@@ -1,29 +1,39 @@
-import { faExclamationCircle, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useBorrowerHttpClient } from "@frontend-monorepo/http-client-borrower";
-import { formatCurrency } from "@frontend-monorepo/ui-shared";
-import { Badge, Box, Button, Text } from "@radix-ui/themes";
+import type { StableCoin } from "@frontend-monorepo/ui-shared";
+import { formatCurrency, StableCoinHelper } from "@frontend-monorepo/ui-shared";
+import { Badge, Box, Button, Callout, Flex, Heading, Separator, Text, Tooltip as Popup } from "@radix-ui/themes";
+import { format } from "date-fns";
 import QRCode from "qrcode.react";
 import { type FormEvent, useState } from "react";
-import { Alert, Col, Container, Form, Row } from "react-bootstrap";
+import { Alert, Container, Form } from "react-bootstrap";
+import { FaInfoCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 interface ContractPrincipalGivenProps {
+  loanAmount: number;
+  interestAmount: number;
   totalRepaymentAmount: number;
   repaymentAddress: string;
   contractId: string;
+  expiry: Date;
+  coin: StableCoin;
 }
 
 export function ContractPrincipalGiven({
+  loanAmount,
+  interestAmount,
   totalRepaymentAmount,
   repaymentAddress,
   contractId,
+  expiry,
+  coin,
 }: ContractPrincipalGivenProps) {
   const navigate = useNavigate();
 
   const [txid, setTxid] = useState("");
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const { markAsRepaymentProvided } = useBorrowerHttpClient();
   const [submitted, setSubmitted] = useState(false);
@@ -32,13 +42,8 @@ export function ContractPrincipalGiven({
     e.preventDefault();
     try {
       setError("");
-      setSuccess("");
       setIsLoading(true);
       await markAsRepaymentProvided(contractId, txid);
-      setSuccess(
-        "Contract has been marked as repaid. Once the lender confirms the repayment, you will be\n"
-          + "able to withdraw the collateral from the contract.",
-      );
       setSubmitted(true);
       setTimeout(() => {
         navigate(0);
@@ -64,48 +69,112 @@ export function ContractPrincipalGiven({
 
   return (
     <Container fluid>
-      <Row>
-        <h4 className={"text-font dark:text-font-dark"}>Repayment Details</h4>
-      </Row>
+      <Box>
+        <Heading className={"text-font dark:text-font-dark"} size={"4"} weight={"medium"}>
+          Repay your Loan
+        </Heading>
+        <Separator className="bg-font/10 dark:bg-font-dark/10" size={"4"} my={"4"} />
+        <Box className="space-y-4">
+          <Flex align={"center"} justify={"between"}>
+            <Text size={"2"} weight={"medium"} className="text-font/70 dark:text-font-dark/70 shrink-0">
+              Loan Amount
+            </Text>
 
-      <Row className="mt-4">
-        <Col className="text-center">
-          <div className="d-flex justify-content-center align-items-center flex-column">
-            <Box
-              onClick={() => handleCopy(repaymentAddress)}
-              p={"5"}
-              className="rounded-2xl bg-white cursor-copy hover:shadow-sm"
+            <Text size={"2"} weight={"medium"} className="text-end text-font/70 dark:text-font-dark/70">
+              {formatCurrency(loanAmount)}
+            </Text>
+          </Flex>
+          <Flex align={"center"} justify={"between"}>
+            <Text size={"2"} weight={"medium"} className="text-font/70 dark:text-font-dark/70 shrink-0">
+              Interest Amount
+            </Text>
+            <Text
+              size={"2"}
+              weight={"medium"}
+              className="text-end text-font/70 dark:text-font-dark/70"
             >
-              <QRCode value={repaymentAddress} size={200} renderAs={"svg"} />
-            </Box>
-            <div className="d-flex justify-content-center align-items-center">
-              <div className="p-4 d-flex align-items-center flex-wrap gap-2">
-                <p
+              {formatCurrency(interestAmount)}
+            </Text>
+          </Flex>
+          <Separator className="bg-font/10 dark:bg-font-dark/10" size={"4"} my={"4"} />
+          <Flex align={"center"} justify={"between"}>
+            <Text size={"2"} weight={"bold"} className="shrink-0 text-font dark:text-font-dark">
+              Total Owed
+            </Text>
+
+            <Text size={"2"} weight={"bold"} className="text-end text-font dark:text-font-dark">
+              {formatCurrency(totalRepaymentAmount)}
+            </Text>
+          </Flex>
+          <Separator className="bg-font/10 dark:bg-font-dark/10" size={"4"} my={"4"} />
+        </Box>
+
+        <Box py={"4"} className="text-center">
+          <Callout.Root color={"blue"}>
+            <Callout.Icon>
+              <FaInfoCircle size={"18"} />
+            </Callout.Icon>
+            <Callout.Text>
+              You are expected to pay back your loan by <strong>{format(expiry, "PPPP")}</strong>. Remember to{" "}
+              <strong>pay back in full</strong>, with a single transaction; partial repayments are not supported. You
+              must pay back using the same asset you borrowed:{" "}
+              <a
+                href={StableCoinHelper.toContractUrl(coin)}
+                className="text-blue-500 hover:underline hover:text-blue-700"
+              >
+                {StableCoinHelper.print(coin)}
+              </a>.
+            </Callout.Text>
+          </Callout.Root>
+        </Box>
+
+        <Box py={"4"} className="text-center">
+          <Text size={"2"} weight={"medium"} className="text-font/60 dark:text-font-dark/60">
+            Scan QR code to make payment
+          </Text>
+        </Box>
+        <Flex align={"center"} justify={"center"} direction={"column"} gap={"4"}>
+          <Box
+            onClick={() => handleCopy(repaymentAddress)}
+            p={"5"}
+            className="rounded-2xl bg-white cursor-copy hover:shadow-sm"
+          >
+            <QRCode renderAs={"svg"} value={repaymentAddress} size={300} />
+          </Box>
+          <Flex
+            align={"center"}
+            justify={"center"}
+            direction={"column"}
+            gap={"3"}
+          >
+            <Text
+              size={"2"}
+              className="text-font/60 dark:text-font-dark/60 text-center max-w-sm font-medium"
+            >
+              Please send <em>exactly</em>{"  "}
+              <Popup
+                content={"Copy exact amount to send"}
+                className="text-font dark:text-font-dark font-semibold"
+              >
+                <span
                   onClick={() => handleCopy(totalRepaymentAmount.toString())}
-                  className="text-break font-semibold cursor-copy m-0 text-font dark:text-font-dark"
+                  className="text-font dark:text-font-dark font-semibold cursor-copy"
                 >
-                  To receive back your collateral please send
-                </p>
-                <p
-                  onClick={() => handleCopy(totalRepaymentAmount.toString())}
-                  className="text-break font-semibold cursor-copy m-0 text-font dark:text-font-dark"
-                >
-                  <strong>{formatCurrency(totalRepaymentAmount)}</strong>
-                </p>
-                <p
-                  onClick={() => handleCopy(totalRepaymentAmount.toString())}
-                  className="text-break font-semibold cursor-copy m-0 text-font dark:text-font-dark"
-                >
-                  to
-                </p>
-                <p
-                  onClick={() => handleCopy(repaymentAddress)}
-                  className="text-break font-semibold cursor-copy m-0 text-font dark:text-font-dark"
-                >
+                  {formatCurrency(totalRepaymentAmount)} {"  "}
+                </span>
+              </Popup>
+              to{"  "}<br />
+              <Button
+                onClick={() => handleCopy(repaymentAddress)}
+                asChild
+                variant="ghost"
+                className="cursor-copy mt-1"
+              >
+                <span className="text-font dark:text-font-dark font-semibold">
                   {repaymentAddress}
-                </p>
-              </div>
-            </div>
+                </span>
+              </Button>
+            </Text>
             <Badge radius="full" color={copied ? "green" : "gray"}>
               <Text size={"1"}>
                 {!copied
@@ -113,33 +182,44 @@ export function ContractPrincipalGiven({
                   : "Copied to clipboard!"}
               </Text>
             </Badge>
-          </div>
-        </Col>
-      </Row>
+          </Flex>
+        </Flex>
+      </Box>
 
-      <Row className="mt-2">
-        <Col>
-          <Alert variant="warning">
-            <FontAwesomeIcon icon={faInfoCircle} /> Please send the amount in a single transaction.
-          </Alert>
-        </Col>
-      </Row>
+      <Separator className="bg-font/10 dark:bg-font-dark/10" size={"4"} my={"4"} />
+
+      <Box py={"4"} className="text-center">
+        <Callout.Root color={"teal"}>
+          <Callout.Icon>
+            <FaInfoCircle size={"18"} />
+          </Callout.Icon>
+          <Callout.Text>
+            After sending the repayment amount to the address above, you <em>must</em>{" "}
+            confirm the repayment by providing the <strong>repayment transaction ID</strong>. Make sure to provide the
+            {" "}
+            <strong>correct</strong>{" "}
+            transaction ID, to allow the lender to verify the repayment. Once the lender confirms your repayment, you
+            will be able to claim your collateral.
+          </Callout.Text>
+        </Callout.Root>
+      </Box>
 
       <Form onSubmit={onSubmit}>
         <Form.Group controlId="formTxId" className="mb-3">
-          <Form.Label className={"text-font dark:text-font-dark"} column={"sm"}>Provide the transaction id</Form.Label>
+          <Form.Label className={"text-font dark:text-font-dark"} column={"sm"}>Repayment Transaction ID</Form.Label>
           <Form.Control
             type="text"
-            placeholder="0x.."
-            className="p-3 bg-light dark:bg-dark text-font dark:text-font-dark"
+            placeholder="e.g. 0x1b3b3d48df236c1e83ab5e7253f885a6f60699963691ad066aa3a5ae3b298d62"
+            className="p-3 bg-light dark:bg-dark text-font dark:text-font-dark dark:placeholder-gray-500"
             style={{ width: "100%" }}
             value={txid}
+            required={true}
             onChange={(e) => setTxid(e.target.value)}
             disabled={submitted}
           />
         </Form.Group>
         <Button type="submit" loading={isLoading} disabled={submitted}>
-          I've made the payment
+          Confirm Repayment
         </Button>
       </Form>
 
@@ -147,12 +227,6 @@ export function ContractPrincipalGiven({
         <Alert variant="danger">
           <FontAwesomeIcon icon={faExclamationCircle} className="h-4 w-4 mr-2" />
           {error}
-        </Alert>
-      )}
-      {success && (
-        <Alert variant="info">
-          <FontAwesomeIcon icon={faExclamationCircle} className="h-4 w-4 mr-2" />
-          {success}
         </Alert>
       )}
     </Container>
