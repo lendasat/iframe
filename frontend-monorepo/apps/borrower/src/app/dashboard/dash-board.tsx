411cc3a519722fc311d447fb67553557e3e63deb
Lord Lendalot <lord_lendalot@lendasat.com>
2024-12-30 17:22:20 +1100

    chore(dashboard): redesign borrower dashboard step 2

--- frontend-monorepo/apps/borrower/src/app/dashboard/dash-board.tsx
+++ frontend-monorepo/apps/borrower/src/app/dashboard/dash-board.tsx
@@ -1,6 +1,6 @@
 import { ContractStatus, useBorrowerHttpClient } from "@frontend-monorepo/http-client-borrower";
 import { formatCurrency } from "@frontend-monorepo/ui-shared";
-import { Box, Button, Grid, Heading, Skeleton, Text } from "@radix-ui/themes";
+import { Box, Button, Grid, Heading, Skeleton, Tabs, Text } from "@radix-ui/themes";
 import type { HTMLAttributeAnchorTarget } from "react";
 import type { IconType } from "react-icons";
 import { BsBank, BsTicketPerforatedFill } from "react-icons/bs";
@@ -39,20 +39,41 @@ function DashBoard() {
 
   return (
     <Box
-      className="flex flex-col overflow-y-scroll p-4 md:p-8 dark:bg-dark"
+      className="flex flex-col overflow-y-scroll p-4 dark:bg-dark"
       height={innerHeight - 120 + "px"}
     >
-      <Grid className="sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 lg:grid-cols-3 gap-5">
-        <Box className="bg-white dark:bg-dark-700 rounded-2xl p-5 min-h-72 h-full">
+      <Grid
+        className="w-full"
+        columns={{
+          initial: "1",
+          sm: "3",
+          md: "3",
+          lg: "3",
+          xl: "3",
+        }}
+        gapX="4"
+        gapY="4"
+      >
+        <Box
+          id="summary"
+          gridColumnStart={{
+            initial: "1",
+          }}
+          className="bg-white dark:bg-dark-700 rounded-2xl p-5 min-h-72 h-full"
+        >
           <Text as="p" weight={"medium"} className="text-font dark:text-font-dark" size={"3"}>Open Contracts</Text>
-          {/* Total Loan Received */}
+          {/*Total Loan Received*/}
           <Heading size={"8"} mt={"3"} className="text-font dark:text-font-dark">
             {loading ? <Skeleton>$756,809.32</Skeleton> : formatCurrency(totalLoanAmount)}
           </Heading>
 
-          <Box className="grid grid-cols-2 gap-3 mt-8">
+          <Grid
+            columns={{ initial: "1", md: "2" }}
+            gap={"3"}
+            className="col-span-2 mt-8"
+          >
             <Box className="border border-font/10 dark:border-font-dark/20 rounded-xl py-2 px-3">
-              {/* Total number of loans received */}
+              {/*Total number of loans received*/}
               <Heading className="text-font dark:text-font-dark" size={"6"}>
                 {loading ? <Skeleton>6</Skeleton> : totalLoans}
               </Heading>
@@ -60,18 +81,34 @@ function DashBoard() {
             </Box>
 
             <Box className="border border-font/10 dark:border-font-dark/20 rounded-xl py-2 px-3">
-              {/* Total number of loans not repaid/closed */}
+              {/*Total number of loans not repaid/closed */}
               <Heading className="text-font dark:text-font-dark" size={"6"}>
                 {loading ? <Skeleton>2</Skeleton> : totalActiveLoans}
               </Heading>
               <Text size={"2"} weight={"medium"} className="text-font/70 dark:text-font-dark/70">Open Contracts</Text>
             </Box>
-          </Box>
+          </Grid>
         </Box>
-        <Box className="bg-white dark:bg-dark-700 rounded-2xl p-5 md:col-span-2 min-h-72 h-full">
+
+        <Box
+          id="quick_actions"
+          gridColumnStart={{
+            initial: "1",
+            sm: "2",
+          }}
+          gridColumnEnd={{
+            initial: "2",
+            sm: "4",
+          }}
+          className="bg-white dark:bg-dark-700 rounded-2xl p-5 min-h-72 h-full"
+        >
           {/* Quick action buttons */}
           <Text as="p" weight={"medium"} className="text-font dark:text-font-dark" size={"3"}>Quick Actions</Text>
-          <Box className="grid xl:grid-cols-4 grid-cols-2 gap-y-5 gap-x-8 px-3 mt-5">
+          <Grid
+            columns={{ initial: "2", sm: "4" }}
+            width="auto"
+            className="gap-y-5 gap-x-8 px-3 mt-5"
+          >
             <QuickLinks
               Icon={BsTicketPerforatedFill}
               url="/requests"
@@ -79,7 +116,6 @@ function DashBoard() {
               label="Request Loan"
               target={"_self"}
             />
-
             <QuickLinks
               Icon={IoWalletOutline}
               url="/my-contracts"
@@ -101,7 +137,44 @@ function DashBoard() {
               label="Help Center"
               target={"_blank"}
             />
-          </Box>
+          </Grid>
+        </Box>
+
+        <Box
+          id="contracts"
+          gridColumnStart={{
+            initial: "1",
+          }}
+          gridColumnEnd={{
+            initial: "2",
+            sm: "4",
+          }}
+          className="bg-white dark:bg-dark-700 rounded-2xl p-5 min-h-72 h-full"
+        >
+          <Text as="p" weight={"medium"} className="text-font dark:text-font-dark" size={"3"}>Contracts</Text>
+          <Tabs.Root defaultValue="open">
+            <Tabs.List size="2" color="blue">
+              <Tabs.Trigger value="open">
+                Open
+              </Tabs.Trigger>
+              <Tabs.Trigger value="closed" disabled={true}>Closed</Tabs.Trigger>
+              <Tabs.Trigger value="all" disabled={true}>All</Tabs.Trigger>
+            </Tabs.List>
+
+            <Box pt="3">
+              <Tabs.Content value="open">
+                <Text size="2">Open contracts</Text>
+              </Tabs.Content>
+
+              <Tabs.Content value="closed">
+                <Text size="2">Closed contracts</Text>
+              </Tabs.Content>
+
+              <Tabs.Content value="all">
+                <Text size="2">All contracts</Text>
+              </Tabs.Content>
+            </Box>
+          </Tabs.Root>
         </Box>
       </Grid>
     </Box>
