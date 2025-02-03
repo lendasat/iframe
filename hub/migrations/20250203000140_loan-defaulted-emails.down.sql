03554d8301c2a4b9ae09db40feb42963c358e461
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2025-02-03 18:03:33 +1100

    fix(hub): Do not add defaulted_loan_liquidated_sent again

--- hub/migrations/20250203000140_loan-defaulted-emails.down.sql
+++ hub/migrations/20250203000140_loan-defaulted-emails.down.sql
@@ -2,5 +2,3 @@ ALTER TABLE contract_emails
     DROP COLUMN IF EXISTS defaulted_loan_borrower_sent;
 ALTER TABLE contract_emails
     DROP COLUMN IF EXISTS defaulted_loan_lender_sent;
-ALTER TABLE contract_emails
-    DROP COLUMN IF EXISTS defaulted_loan_liquidated_sent;
