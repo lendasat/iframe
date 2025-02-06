b0700a9e4755731b972c08f73ec9df5e5c98fbc0
lord lendalot <lord_lendalot@lendasat.com>
2025-02-06 11:19:38 +1100

    feat: add average APR to lender dashboard

--- frontend-monorepo/apps/lender/src/app/dashboard/dashboard.tsx
+++ frontend-monorepo/apps/lender/src/app/dashboard/dashboard.tsx
@@ -1,4 +1,4 @@
-import { ContractStatus, useLenderHttpClient } from "@frontend-monorepo/http-client-lender";
+import { Contract, ContractStatus, useLenderHttpClient } from "@frontend-monorepo/http-client-lender";
 import { formatCurrency, ONE_YEAR } from "@frontend-monorepo/ui-shared";
 import { InfoCircledIcon } from "@radix-ui/react-icons";
 import { Box, Button, Callout, Flex, Grid, Heading, Text } from "@radix-ui/themes";
@@ -7,90 +7,33 @@ import { useNavigate } from "react-router-dom";
 import { useAsync } from "react-use";
 import DashboardContracts from "./dashoard-contracts";
 
-function Dashboard() {
-  const { innerHeight } = window;
-
-  const { getContracts } = useLenderHttpClient();
-
-  const [hasMnemonicBackedUp, setHasMnemonicBackedUp] = useState(false);
-
-  const { value: maybeContracts } = useAsync(async () => {
-    return await getContracts();
-  }, []);
-
-  useEffect(() => {
-    const storedBackup = localStorage.getItem("mnemonicBackedUp");
-    if (storedBackup) {
-      setHasMnemonicBackedUp(JSON.parse(storedBackup));
-    }
-  }, []);
-
-  const contracts = maybeContracts || [];
-
-  const openContracts = contracts
-    .filter((contract) => contract.status === ContractStatus.PrincipalGiven);
-
-  const numberOfOpenContracts = openContracts.length;
-  const totalLoanAmount = openContracts
-    .reduce((sum, contract) => sum + contract.loan_amount, 0);
-  const totalOpenInterest = openContracts.reduce((sum, contract) =>
-    sum + (
-      contract.loan_amount * (contract.interest_rate / ONE_YEAR * contract.duration_days)
-    ), 0);
-
-  const totalClosedInterest = contracts.filter((contract) =>
-    contract.status === ContractStatus.Closed || contract.status === ContractStatus.Closing
-  ).reduce((sum, contract) =>
-    sum + (
-      contract.loan_amount * (contract.interest_rate / ONE_YEAR * contract.duration_days)
-    ), 0);
-
-  return (
-    <Box
-      className="flex flex-col overflow-y-scroll p-4 md:p-8 dark:bg-dark"
-      height={innerHeight - 120 + "px"}
-    >
-      <Box className="space-y-8">
-        <Grid className="md:grid-cols-2 xl:grid-cols-4 gap-5">
-          <QuickBoards
-            color="orange"
-            label="Total Assets in Open Loan"
-            value={totalLoanAmount}
-          />
-          <QuickBoards
-            color="green"
-            label="Number of Active Loans"
-            value={numberOfOpenContracts}
-            numbers={true}
-          />
-          <QuickBoards
-            color="purple"
-            label="Open Interest"
-            value={totalOpenInterest}
-          />
-          <QuickBoards
-            color="brown"
-            label="Earned Interest"
-            value={totalClosedInterest}
-          />
-        </Grid>
-        {!hasMnemonicBackedUp && <AlertBoard color={"red"} />}
-
-        <DashboardContracts contracts={contracts} />
-      </Box>
-    </Box>
-  );
+enum QuickBoardsType {
+  Dollar,
+  Number,
+  Percentage,
 }
 
-export default Dashboard;
-
-interface QuickBoardsType {
+interface QuickBoardsProps {
   color: string;
   label: string;
   value: number;
-  numbers?: boolean;
+  type: QuickBoardsType;
 }
-const QuickBoards = ({ color, label, value, numbers }: QuickBoardsType) => {
+
+const QuickBoards = ({ color, label, value, type }: QuickBoardsProps) => {
+  let formattedValue = value.toFixed();
+  switch (type) {
+    case QuickBoardsType.Dollar:
+      formattedValue = formatCurrency(value);
+      break;
+    case QuickBoardsType.Number:
+      formattedValue = value.toFixed();
+      break;
+    case QuickBoardsType.Percentage:
+      formattedValue = `${(value * 100).toFixed(1)}%`;
+      break;
+  }
+
   return (
     <Box className="min-h-24 bg-white dark:bg-dark-500 py-4 rounded-2xl space-y-4 drop-shadow-sm">
       <Flex className={`relative px-4`} align={"start"} justify={"between"}>
@@ -105,7 +48,7 @@ const QuickBoards = ({ color, label, value, numbers }: QuickBoardsType) => {
             {label}
           </Text>
           <Heading size={"6"} weight={"bold"} className="text-black dark:text-white">
-            {numbers ? value : formatCurrency(value)}
+            {formattedValue}
           </Heading>
         </Box>
       </Flex>
@@ -149,3 +92,105 @@ const AlertBoard = ({ color }: AlertBoardProps) => {
     </Box>
   );
 };
+
+const calculateWeightedAverageAPR = (contracts: Contract[]) => {
+  let totalWeightedValue = 0;
+  let totalLoanAmount = 0;
+
+  contracts.forEach(contract => {
+    totalWeightedValue += contract.interest_rate * contract.loan_amount;
+    totalLoanAmount += contract.loan_amount;
+  });
+
+  return totalWeightedValue / totalLoanAmount;
+};
+
+function Dashboard() {
+  const { innerHeight } = window;
+
+  const { getContracts } = useLenderHttpClient();
+
+  const [hasMnemonicBackedUp, setHasMnemonicBackedUp] = useState(false);
+
+  const { value: maybeContracts } = useAsync(async () => {
+    return await getContracts();
+  }, []);
+
+  useEffect(() => {
+    const storedBackup = localStorage.getItem("mnemonicBackedUp");
+    if (storedBackup) {
+      setHasMnemonicBackedUp(JSON.parse(storedBackup));
+    }
+  }, []);
+
+  const contracts = maybeContracts || [];
+
+  const openContracts = contracts
+    .filter((contract) => contract.status === ContractStatus.PrincipalGiven);
+
+  const numberOfOpenContracts = openContracts.length;
+  const totalLoanAmount = openContracts
+    .reduce((sum, contract) => sum + contract.loan_amount, 0);
+  const totalOpenInterest = openContracts.reduce((sum, contract) =>
+    sum + (
+      contract.loan_amount * (contract.interest_rate / ONE_YEAR * contract.duration_days)
+    ), 0);
+
+  const closedContracts = contracts.filter((contract) =>
+    contract.status === ContractStatus.Closed || contract.status === ContractStatus.Closing
+    || contract.status === ContractStatus.Extended
+  );
+  const totalClosedInterest = closedContracts.reduce((sum, contract) =>
+    sum + (
+      contract.loan_amount * (contract.interest_rate / ONE_YEAR * contract.duration_days)
+    ), 0);
+
+  const averageApr = calculateWeightedAverageAPR(closedContracts);
+
+  return (
+    <Box
+      className="flex flex-col overflow-y-scroll p-4 md:p-8 dark:bg-dark"
+      height={innerHeight - 120 + "px"}
+    >
+      <Box className="space-y-8">
+        <Grid className="md:grid-cols-2 xl:grid-cols-5 gap-5">
+          <QuickBoards
+            color="orange"
+            label="$ in Open Loans"
+            value={totalLoanAmount}
+            type={QuickBoardsType.Dollar}
+          />
+          <QuickBoards
+            color="green"
+            label="Number of Active Loans"
+            value={numberOfOpenContracts}
+            type={QuickBoardsType.Number}
+          />
+          <QuickBoards
+            color="purple"
+            label="Open Interest"
+            value={totalOpenInterest}
+            type={QuickBoardsType.Dollar}
+          />
+          <QuickBoards
+            color="brown"
+            label="Earned Interest"
+            value={totalClosedInterest}
+            type={QuickBoardsType.Dollar}
+          />
+          <QuickBoards
+            color="yellow"
+            label="Average APR"
+            value={averageApr}
+            type={QuickBoardsType.Percentage}
+          />
+        </Grid>
+        {!hasMnemonicBackedUp && <AlertBoard color={"red"} />}
+
+        <DashboardContracts contracts={contracts} />
+      </Box>
+    </Box>
+  );
+}
+
+export default Dashboard;
