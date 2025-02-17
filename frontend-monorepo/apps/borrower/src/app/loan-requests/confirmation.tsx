95aa48f583a8ddc3f364d242c41d893d3ff862ce
lord lendalot <lord_lendalot@lendasat.com>
2025-02-17 12:08:41 +1100

    fix: don't show address input for card

--- frontend-monorepo/apps/borrower/src/app/loan-requests/confirmation.tsx
+++ frontend-monorepo/apps/borrower/src/app/loan-requests/confirmation.tsx
@@ -274,10 +274,17 @@ export const Confirmation = ({
     loanAsset &&
     LoanAssetHelper.isFiat(loanAsset) &&
     !encryptedFiatTransferDetails;
-  const kycButNoKycConfirmed =
-    selectedOffer?.kyc_link !== undefined && !kycFormDialogConfirmed;
+  const kycButNoKycConfirmed = Boolean(
+    selectedOffer?.kyc_link && !kycFormDialogConfirmed,
+  );
   const buttonDisabled =
     isStillLoading || fiatButNoEncryptedDataPresent || kycButNoKycConfirmed;
+
+  const showStablecoinLoadAddressInput = Boolean(
+    selectedOffer?.loan_asset &&
+      LoanAssetHelper.isStableCoin(selectedOffer.loan_asset) &&
+      selectedProduct !== LoanProductOption.PayWithMoonDebitCard,
+  );
 
   return (
     <Grid
@@ -635,35 +642,34 @@ export const Confirmation = ({
                 </DataList.Value>
               </DataList.Item>
             )}
-            {selectedOffer?.loan_asset &&
-              LoanAssetHelper.isStableCoin(selectedOffer.loan_asset) && (
-                <DataList.Item>
-                  <DataList.Label minWidth="88px">Loan address</DataList.Label>
-                  <DataList.Value className="w-full">
-                    {isStillLoading || !loanAsset ? (
-                      <Skeleton loading={isStillLoading}>Loading</Skeleton>
-                    ) : (
-                      <Flex direction={"column"}>
-                        <LoanAddressInputField
-                          loanAddress={loanAddress ?? ""}
-                          setLoanAddress={setLoanAddress}
-                          hideButton={hideWalletConnectButton}
-                          setHideButton={setHideWalletConnectButton}
-                          loanAsset={loanAsset}
-                          renderWarning={true}
-                        />
-                        <Text
-                          size={"1"}
-                          weight={"light"}
-                          className="text-font dark:text-font-dark"
-                        >
-                          This address will be used to transfer the loan amount
-                        </Text>
-                      </Flex>
-                    )}
-                  </DataList.Value>
-                </DataList.Item>
-              )}
+            {showStablecoinLoadAddressInput && (
+              <DataList.Item>
+                <DataList.Label minWidth="88px">Loan address</DataList.Label>
+                <DataList.Value className="w-full">
+                  {isStillLoading || !loanAsset ? (
+                    <Skeleton loading={isStillLoading}>Loading</Skeleton>
+                  ) : (
+                    <Flex direction={"column"}>
+                      <LoanAddressInputField
+                        loanAddress={loanAddress ?? ""}
+                        setLoanAddress={setLoanAddress}
+                        hideButton={hideWalletConnectButton}
+                        setHideButton={setHideWalletConnectButton}
+                        loanAsset={loanAsset}
+                        renderWarning={true}
+                      />
+                      <Text
+                        size={"1"}
+                        weight={"light"}
+                        className="text-font dark:text-font-dark"
+                      >
+                        This address will be used to transfer the loan amount
+                      </Text>
+                    </Flex>
+                  )}
+                </DataList.Value>
+              </DataList.Item>
+            )}
           </DataList.Root>
           <Button
             size={"3"}
