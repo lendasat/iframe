e67a17040b0c5dc7026875e52494dc731739be25
lord lendalot <lord_lendalot@lendasat.com>
2025-03-25 09:32:04 +1100

    chore: still working

---
+++ frontend-monorepo/apps/button/jest.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default {
+  displayName: "button",
+  transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
+    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nx/react/babel"] }],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+};
--- frontend-monorepo/apps/button/vite.config.ts
+++ frontend-monorepo/apps/button/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,5 +21,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-
-})
+});
