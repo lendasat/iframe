8eb532dc09e51859b7668bdb0f3689eb997cd9cf
lord lendalot <lord_lendalot@lendasat.com>
2025-03-25 09:42:20 +1100

    chore: just playing around with shadcn

--- frontend-monorepo/apps/button/src/App.tsx
+++ frontend-monorepo/apps/button/src/App.tsx
@@ -1,14 +1,38 @@
-import './App.css'
-import { Button } from "@/components/ui/button"
-
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 function App() {
-
   return (
-        <Button onClick={() => console.log("clicked")}>
-          Click me!
-        </Button>
-  )
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button>Open</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
 
 export default App;
