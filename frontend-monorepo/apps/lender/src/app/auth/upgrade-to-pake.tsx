f3c0c71c5112cca78bc6d60cb996a5144e8b2189
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2025-05-13 09:01:10 +1000

    fix(borrower): Use correct HTTP client for PAKE upgrade
    The lender can still use the old, generic one. The borrower has to use
    their new dedicated client.

--- frontend-monorepo/apps/lender/src/app/auth/upgrade-to-pake.tsx
+++ frontend-monorepo/apps/lender/src/app/auth/upgrade-to-pake.tsx
@@ -1,10 +1,20 @@
+import { useBaseHttpClient } from "@frontend/base-http-client";
 import { useAuth } from "@frontend/http-client-lender";
 import { UpgradeToPake as UpgradeToPakeGeneric } from "@frontend/ui-shared";
 
 function UpgradeToPake() {
   const { login } = useAuth();
 
-  return <UpgradeToPakeGeneric login={login} is_borrower={false} />;
+  const { upgradeToPake, finishUpgradeToPake } = useBaseHttpClient();
+
+  return (
+    <UpgradeToPakeGeneric
+      login={login}
+      is_borrower={false}
+      upgradeToPake={upgradeToPake}
+      finishUpgradeToPake={finishUpgradeToPake}
+    />
+  );
 }
 
 export default UpgradeToPake;
