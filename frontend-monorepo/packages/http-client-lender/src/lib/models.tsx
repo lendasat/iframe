c06f756e4bc1153174385fc17e0f247f2f70af35
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2025-06-14 00:32:33 +1000

    chore(frontend): Align terminology around monthly interest loans

--- frontend-monorepo/packages/http-client-lender/src/lib/models.tsx
+++ frontend-monorepo/packages/http-client-lender/src/lib/models.tsx
@@ -227,9 +227,9 @@ export enum RepaymentPlan {
 export function repaymentPlanLabel(plan: RepaymentPlan): string {
   switch (plan) {
     case RepaymentPlan.InterestOnlyMonthly:
-      return "Interest-Only Monthly";
+      return "Monthly Interest";
     case RepaymentPlan.InterestOnlyWeekly:
-      return "Interest-Only Weekly";
+      return "Weekly Interest";
     case RepaymentPlan.Bullet:
       return "Bullet";
   }
