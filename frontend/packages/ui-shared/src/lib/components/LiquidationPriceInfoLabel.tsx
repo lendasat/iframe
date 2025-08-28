9e96a459eee4074a3d664150383140585dee752b
lord lendalot <lord_lendalot@lendasat.com>
2025-08-28 12:25:55 +1000

    fix: show correct info

--- frontend/packages/ui-shared/src/lib/components/LiquidationPriceInfoLabel.tsx
+++ frontend/packages/ui-shared/src/lib/components/LiquidationPriceInfoLabel.tsx
@@ -16,7 +16,7 @@ export function LiquidationPriceInfoLabel({
       subHeader={""}
       description={
         "If the price of Bitcoin falls to a predetermined liquidation level, it triggers a liquidation event, and the collateral is liquidated." +
-        "Liquidation price equals loan-to-value (LTV) of 95%. If the Bitcoin price hits the liquidation price, even for only a couple of seconds, it is considered as a liquidation event."
+        "Liquidation price equals loan-to-value (LTV) of 90%. If the Bitcoin price hits the liquidation price, even for only a couple of seconds, it is considered as a liquidation event."
       }
     >
       <RadixLink href="https://faq.lendasat.com" target="_blank">
